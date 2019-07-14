bd400f6e4259d9a9063956acd8eaba0897c17f42
Dan Kreiger <dan@dankreiger.com>
2019-07-14 13:30:36 +0200

    feat(form): add form with nextjs

---
+++ components/LoginForm/LoginForm.tsx
@@ -0,0 +1,49 @@
+import { useState } from 'react';
+import { Button } from 'puppy-components';
+
+interface ILoginFormState {
+  email: string;
+  password: string;
+}
+
+const LoginForm: React.SFC = () => {
+  const [formValues, setFormValues] = useState<ILoginFormState>({
+    email: '',
+    password: ''
+  });
+  const handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void = e => {
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
+  const handleSubmit: (e: React.FormEvent<HTMLFormElement>) => void = e => {
+    e.preventDefault();
+    console.log(formValues);
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <input
+          type="email"
+          name="email"
+          placeholder="email"
+          onChange={handleChange}
+          value={formValues.email}
+        />
+      </div>
+      <div>
+        <input
+          type="password"
+          name="password"
+          placeholder="password"
+          onChange={handleChange}
+          value={formValues.password}
+        />
+      </div>
+      <Button type="submit" text="Submit" size="medium">
+        Submit
+      </Button>
+    </form>
+  );
+};
+
+export default LoginForm;
---
+++ pages/login/login.tsx
@@ -0,0 +1,5 @@
+import LoginForm from '../../components/LoginForm/LoginForm';
+
+const login: () => JSX.Element = () => <LoginForm />;
+
+export default login;
